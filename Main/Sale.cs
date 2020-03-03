64870e7262fa79af6c1fb29ce2a5688759c5917b
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-03 20:54:35 +0100

    Add Sale Class

---
+++ Main/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    class Sale
+    {
+        public string SaleID { get; private set; }
+        public string SaleDate { get; set; }
+        public string EmployeeID { get; set; }
+        public string CustomerID { get; set; }
+        public Sale(string saleDate, string customerID, string employeeID)
+        {
+            this.SaleDate = saleDate;
+            this.EmployeeID = employeeID;
+            this.CustomerID = customerID;
+        }
+    }
+}
