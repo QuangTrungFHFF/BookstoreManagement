69a7cd21f0c061667fc0f48ccd4934e442152e1f
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-03 21:07:07 +0100

    Add SaleDetail class

---
+++ Main/SaleDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    class SaleDetail
+    {
+        public Guid ID { get; private set; }
+        public string SaleID { get; set; }
+        public string ProductID { get; set; }
+        public string Quantity { get; set; }
+
+        public SaleDetail(string saleID, string productID, string quantity)
+        {
+            this.ID = new Guid();
+            this.SaleID = saleID;
+            this.ProductID = productID;
+            this.Quantity = quantity;
+        }
+    }
+}
