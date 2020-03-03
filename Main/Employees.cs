9ad188849bb0dc8443e620a3235451a18f06acec
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-03 20:40:32 +0100

    Add Employee class

---
+++ Main/Employees.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    class Employees
+    {
+        public string EmployeeID { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; private set; }
+        public string JobTitle { get; private set; }
+        public string Branch {get; set;}
+        public string Department { get; set; }
+        public string BussinessPhoneNumber { get; set; }
+        public string HomePhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Nationality { get; set; }
+
+        public Employees(string employeeID, string employeeFirstName, string employeeLastName,
+            string username, string password, string jobTitle, string branch, string department,
+            string bussinessPhoneNumber, string homePhoneNumber, string address,
+            string city, string nationality)
+        {
+            this.EmployeeID = employeeID;
+            this.EmployeeFirstName = employeeFirstName;
+            this.EmployeeLastName = employeeLastName;
+            this.UserName = username;
+            this.Password = password;
+            this.JobTitle = jobTitle;
+            this.Branch = branch;
+            this.Department = department;
+            this.BussinessPhoneNumber = bussinessPhoneNumber;
+            this.HomePhoneNumber = homePhoneNumber;
+            this.Address = address;
+            this.City = city;
+            this.Nationality = nationality;
+        }
+
+
+    }
+}
