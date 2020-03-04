8dc3e9f53eb22aca9f1f3fdc6624cec2db710857
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-04 21:38:40 +0100

    Add book class

---
+++ Main/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    class Book
+    {
+        public string ProductID { get; set; }
+        public string BookID { get; set; }
+        public string Gender { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Year { get; set; }
+        public string Isdn { get; set; }
+    }
+}
