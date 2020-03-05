05d4eb6458f1e39bf4fcb5043ad31f6fc1a9ef8a
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-05 22:16:02 +0100

    Update book class

--- Main/Book.cs
+++ Main/Book.cs
@@ -12,7 +12,24 @@ namespace Main
         public string Title { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
-        public string Year { get; set; }
-        public string Isdn { get; set; }
+        public int Year { get; set; }
+        public string Isbn { get; set; }
+
+        public Book(string productID, string bookID, string gender, string title, string author, string publisher, int year, string isbn)
+        {
+            this.ProductID = productID;
+            this.BookID = bookID;
+            this.Gender = gender;
+            this.Title = title;
+            this.Author = author;
+            this.Publisher = publisher;
+            this.Year = year;
+            this.Isbn = isbn;
+        }
+        public override bool Equals(object obj)
+        {
+            var that = (Book)obj;
+            return this.Isbn.Equals(that.Isbn, StringComparison.OrdinalIgnoreCase);
+        }
     }
 }
