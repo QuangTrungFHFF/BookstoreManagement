dcd24ef395e7c785384c00b3c1a20fb1ebd56cf4
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-07 21:50:22 +0100

    Add sale class

---
+++ FormUI/Sale.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    class Sale
+    {
+        public string SaleID { get; private set; }
+        public string SaleDate { get; set; }
+        public string EmployeeID { get; set; }
+        public string CustomerID { get; set; }
+        public Sale(string saleID, string saleDate, string customerID, string employeeID)
+        {
+            this.SaleID = saleID;
+            this.SaleDate = saleDate;
+            this.EmployeeID = employeeID;
+            this.CustomerID = customerID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var that = (Sale)obj;
+            return this.SaleID.Equals(that.SaleID,StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.SaleID.GetHashCode();
+        }
+    }
+}
