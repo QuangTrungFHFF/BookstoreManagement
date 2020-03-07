3b17f929d19f14f0ede06c5dfa3f58655f53b7a4
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-03-07 21:51:02 +0100

    Add Sale Detail class

---
+++ FormUI/SaleDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    class SaleDetail
+    {
+        public Guid ID { get; private set; }
+        public string SaleID { get; set; }
+        public string ProductID { get; set; }
+        public string Quantity { get; set; }
+
+        public SaleDetail(string saleID, string productID, string quantity)
+        {
+            this.ID = new Guid();
+            this.SaleID = saleID;
+            this.ProductID = productID;
+            this.Quantity = quantity;
+        }
+        public override bool Equals(object obj)
+        {
+            var that = (SaleDetail)obj;
+            return this.ID.Equals(that.ID);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID.GetHashCode();
+        }
+    }
+}
